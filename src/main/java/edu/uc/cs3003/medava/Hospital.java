cccde984a764f8ebb45439aad17c2bc7b5971530
Jovan Jose Asker Fredy <jovan.asker@gmail.com>
2025-11-19 21:00:04 -0500

    Update Hospital.java
    Implement Hospital class with constructor and receive method

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,1 +1,30 @@
 package edu.uc.cs3003.medava;
+
+public class Hospital {
+
+    private final String name;
+
+    public Hospital(String hospitalName) {
+        name = hospitalName;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    void receive(Transporter t) {
+        while (!t.isEmpty()) {
+            Shippable unloaded = t.unload();
+            System.out.println(String.format("Checking whether Hospital can receive %s.",
+                    unloaded.getMedicineName()));
+            if (unloaded.getSchedule() != MedicineSchedule.Uncontrolled) {
+                System.out.println(String.format(
+                        "Hospital cannot receive controlled substances and %s is a %s.",
+                        unloaded.getMedicineName(), unloaded.getSchedule().asString()));
+            } else {
+                System.out.println(String.format("Accepted a shipment of %s.",
+                        unloaded.getMedicineName()));
+            }
+        }
+    }
+}
