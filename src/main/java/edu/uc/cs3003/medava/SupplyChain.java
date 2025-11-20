a79be44cb62393e5fa545ce3f265f7f33ac9ac0a
Jovan Jose Asker Fredy <jovan.asker@gmail.com>
2025-11-19 21:06:31 -0500

    Update SupplyChain.java

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -1,6 +1,9 @@
 package edu.uc.cs3003.medava;
 
+
 public class SupplyChain {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        // System.out.println("Hello, Jovan Jose Asker Fredy! This is the WOPR speaking.");
+        HospitalRunner.run();
     }
 }
